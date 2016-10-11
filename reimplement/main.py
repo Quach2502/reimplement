b825bd6d24a557da776ddceeb35d0db80d538604
Quach <quachdung2502@gmail.com>
2016-10-11 16:42:34 +0800

    fix

--- reimplement/main.py
+++ reimplement/main.py
@@ -2,11 +2,12 @@ import re
 from nltk.corpus import sentiwordnet as swn
 import nltk
 from nltk import ngrams
+from time import time
 from nltk.parse.stanford import StanfordParser
 from nltk.parse.stanford import StanfordDependencyParser
 from sklearn.svm import LinearSVC
 import string
-from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.preprocessing import Normalizer
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import accuracy_score
@@ -15,11 +16,13 @@ from xml.etree import ElementTree as ET
 from scipy.sparse import csr_matrix
 from scipy.sparse import hstack
 import numpy as np
-path_to_jar = 'C:\Users\t_quacd\AppData\Local\stanford-parser-full-2015-12-09/stanford-parser.jar'
-path_to_models_jar = 'C:\Users\t_quacd\AppData\Local\stanford-parser-full-2015-12-09/stanford-parser-3.6.0-models.jar'
-parser = StanfordParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
-dep_parser = StanfordDependencyParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
-
+from sklearn import grid_search
+from random import randint
+# path_to_jar = 'C:\Users\t_quacd\AppData\Local\stanford-parser-full-2015-12-09/stanford-parser.jar'
+# path_to_models_jar = 'C:\Users\t_quacd\AppData\Local\stanford-parser-full-2015-12-09/stanford-parser-3.6.0-models.jar'
+# parser = StanfordParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
+# dep_parser = StanfordDependencyParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
+#
 stopwords = [var for var in stopwords.words('english') if var not in ['not', 'isn']]
 
 
@@ -68,26 +71,37 @@ def BL_lexicon():
 # def sentimental_lexicon_id(): #in domain lexicon
 
 
-def surface_context(sentence, aspect_term, window_size):  # from_index is the begin of the aspect term in sentence
-    aspect_term_split = aspect_term.split()
+def surface_context(sentence, aspect_term,from_char,to_char, window_size):  # from_index is the begin of the aspect term in list sentence.strip()
     list_sentence = sentence.split()
-    if len(aspect_term_split) == 1:
-        from_index = list_sentence.index(aspect_term_split[0])
+    from_index=0
+    to_index = 0
+    position = 0
+    for each in list_sentence:
+        if (position + len(each) + 1 )<= int(from_char):
+            position += len(each) + 1
+        else:
+            from_index = list_sentence.index(each)
+            break
+    position = 0
+    if len(aspect_term.split()) == 1:
         to_index = from_index
     else:
-        from_index = list_sentence.index(aspect_term_split[0])
-        to_index = list_sentence.index(aspect_term_split[-1])
-    # print from_index, to_index
-    if from_index - window_size > 0:
+        for each in list_sentence:
+            if position + len(each)+ 1 < int(to_char):
+                position += len(each) + 1
+            else:
+                to_index = list_sentence.index(each)
+                break
+    if (from_index - window_size > 0):
         window_begin = from_index - window_size
     else:
         window_begin = 0
     if to_index + window_size < (len(list_sentence)):
         window_end = to_index + window_size
     else:
-        window_end = len(list_sentence) - 1
+        window_end = len(list_sentence)
     # print window_begin,window_end
-    window = list_sentence[window_begin:(window_end + 1)]
+    window = list_sentence[window_begin:window_end]
     return window
 
 
@@ -134,32 +148,27 @@ def lexicon_feature(sentence):
     return result
 
 
-def sentence_transform(sentence, aspect_term, window_size):
-    window = surface_context(sentence, aspect_term, window_size)
-    aspect_term_split = aspect_term.split()
-    if len(aspect_term_split) == 1:
-        from_index = window.index(aspect_term_split[0])
-        to_index = from_index
-    else:
-        from_index = window.index(aspect_term_split[0])
-        to_index = window.index(aspect_term_split[-1])
+def sentence_transform(sentence, aspect_term,from_char,to_char, window_size):
+    window = surface_context(sentence, aspect_term,from_char,to_char, window_size)
+    sentence_window = ' '.join(window)
+    slice = sentence_window.partition(aspect_term)
     result = []
     for i in range(0, len(window)):
         result.append(window[i])
     for i in range(0, len(window)):
-        if i < from_index:
+        if window[i] in slice[0]:
             result.append(window[i] + "_" + aspect_term + "_ct")
-        elif i > to_index:
+        elif window[i] in slice[2]:
             result.append(aspect_term + "_" + window[i] + "_ct")
     bigrams = ngrams(window, 2)
     for bigram in bigrams:
         result.append(bigram[0] + "_" + bigram[1])
-    result.append(aspect_term+"_at")
+    result.append(aspect_term + "_at")
     return ' '.join(result)
 
 
 # def parse_feature(sentence):
-def GetYFromStringLabels (Labels):
+def GetYFromStringLabels(Labels):
     Y = []
     for L in Labels:
         if 'positive' == L:
@@ -183,21 +192,21 @@ def preprocessData():
     for sentence in root.findall('sentence'):
         if sentence.find('aspectTerms') is None:
             continue
-        content = sentence.find('text').text.translate(string.maketrans("", ""),string.punctuation)
+        content = sentence.find('text').text.translate(string.maketrans("", ""), string.punctuation)
         for aspectTerms in sentence.iter('aspectTerms'):
             for aspectTerm in aspectTerms.iter('aspectTerm'):
-                text.append(sentence_transform(content,aspectTerm.get('term').translate(string.maketrans("", ""),string.punctuation),8))
+                text.append(sentence_transform(content, aspectTerm.get('term').translate(string.maketrans("", ""),
+                                                                                         string.punctuation),aspectTerm.get("from"),aspectTerm.get("to"), 8))
                 polarity.append(aspectTerm.get("polarity"))
     LexFeats = [lexicon_feature(Sent) for Sent in text]
     LexFeats = np.array(LexFeats)
     LexFeats = csr_matrix(LexFeats)
-    CountVecter = CountVectorizer( dtype=np.float64, binary=False,max_df=0.95)
+    CountVecter = CountVectorizer(dtype=np.float64, binary=False, max_df=0.95,stop_words= stopwords)
     Y = GetYFromStringLabels(polarity)
     X = CountVecter.fit_transform(text)
     X = Normalizer().fit_transform(X)
-    print 'shape of X matrix before adding lex feats', X.shape
     X = hstack([X, LexFeats])
-    print 'shape of X matrix after adding lex feats', X.shape
+    return X,Y
     # vectorizer
     # vectorizer = TfidfVectorizer(tokenizer=tokenize, sublinear_tf=True, ngram_range=(1, 1),
     #                              stop_words=stopwords,
@@ -208,17 +217,19 @@ def preprocessData():
 
 
 def main():
-    preprocessData()
-    # features_train, features_test, labels_train, labels_test = preprocessData()
-    # clf = svm.SVC()
-    # t0 = time()
-    # clf.fit(features_train, labels_train)
-    # print "training time:", round(time() - t0, 3), "s"
-    # # predict
-    # t0 = time()
-    # pred = clf.predict(features_test)
-    # print "predicting time:", round(time() - t0, 3), "s"  # accuracy
-    # print metrics.accuracy_score(labels_test, pred)
+    t0 = time()
+    X,Y = preprocessData()
+    print "preprocess time:", round(time() - t0, 3), "s"
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=randint(0, 100))
+    clf = LinearSVC(C=0.1,class_weight='balanced')
+    t0 = time()
+    clf.fit(X_train, y_train)
+    print "training time:", round(time() - t0, 3), "s"
+    # predict
+    t0 = time()
+    pred = clf.predict(X_test)
+    print "predicting time:", round(time() - t0, 3), "s"  # accuracy
+    print accuracy_score(y_test, pred)
 
 
 if __name__ == "__main__":
