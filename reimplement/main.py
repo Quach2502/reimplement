d37b0cc941c88b50c33a817790a1a33468439d8c
Quach <quachdung2502@gmail.com>
2016-10-11 17:43:13 +0800

    fix

--- reimplement/main.py
+++ reimplement/main.py
@@ -18,6 +18,7 @@ from scipy.sparse import hstack
 import numpy as np
 from sklearn import grid_search
 from random import randint
+
 # path_to_jar = 'C:\Users\t_quacd\AppData\Local\stanford-parser-full-2015-12-09/stanford-parser.jar'
 # path_to_models_jar = 'C:\Users\t_quacd\AppData\Local\stanford-parser-full-2015-12-09/stanford-parser-3.6.0-models.jar'
 # parser = StanfordParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
@@ -71,13 +72,14 @@ def BL_lexicon():
 # def sentimental_lexicon_id(): #in domain lexicon
 
 
-def surface_context(sentence, aspect_term,from_char,to_char, window_size):  # from_index is the begin of the aspect term in list sentence.strip()
+def surface_context(sentence, aspect_term, from_char, to_char,
+                    window_size):  # from_index is the begin of the aspect term in list sentence.strip()
     list_sentence = sentence.split()
-    from_index=0
+    from_index = 0
     to_index = 0
     position = 0
     for each in list_sentence:
-        if (position + len(each) + 1 )<= int(from_char):
+        if (position + len(each) + 1) <= int(from_char):
             position += len(each) + 1
         else:
             from_index = list_sentence.index(each)
@@ -87,7 +89,7 @@ def surface_context(sentence, aspect_term,from_char,to_char, window_size):  # fr
         to_index = from_index
     else:
         for each in list_sentence:
-            if position + len(each)+ 1 < int(to_char):
+            if position + len(each) + 1 < int(to_char):
                 position += len(each) + 1
             else:
                 to_index = list_sentence.index(each)
@@ -148,8 +150,8 @@ def lexicon_feature(sentence):
     return result
 
 
-def sentence_transform(sentence, aspect_term,from_char,to_char, window_size):
-    window = surface_context(sentence, aspect_term,from_char,to_char, window_size)
+def sentence_transform(sentence, aspect_term, from_char, to_char, window_size):
+    window = surface_context(sentence, aspect_term, from_char, to_char, window_size)
     sentence_window = ' '.join(window)
     slice = sentence_window.partition(aspect_term)
     result = []
@@ -196,17 +198,18 @@ def preprocessData():
         for aspectTerms in sentence.iter('aspectTerms'):
             for aspectTerm in aspectTerms.iter('aspectTerm'):
                 text.append(sentence_transform(content, aspectTerm.get('term').translate(string.maketrans("", ""),
-                                                                                         string.punctuation),aspectTerm.get("from"),aspectTerm.get("to"), 8))
+                                                                                         string.punctuation),
+                                               aspectTerm.get("from"), aspectTerm.get("to"), 10))
                 polarity.append(aspectTerm.get("polarity"))
     LexFeats = [lexicon_feature(Sent) for Sent in text]
     LexFeats = np.array(LexFeats)
     LexFeats = csr_matrix(LexFeats)
-    CountVecter = CountVectorizer(dtype=np.float64, binary=False, max_df=0.95,stop_words= stopwords)
+    CountVecter = CountVectorizer(dtype=np.float64, binary=False, max_df=0.95, stop_words=stopwords)
     Y = GetYFromStringLabels(polarity)
     X = CountVecter.fit_transform(text)
     X = Normalizer().fit_transform(X)
     X = hstack([X, LexFeats])
-    return X,Y
+    return X, Y
     # vectorizer
     # vectorizer = TfidfVectorizer(tokenizer=tokenize, sublinear_tf=True, ngram_range=(1, 1),
     #                              stop_words=stopwords,
@@ -218,18 +221,22 @@ def preprocessData():
 
 def main():
     t0 = time()
-    X,Y = preprocessData()
+    X, Y = preprocessData()
     print "preprocess time:", round(time() - t0, 3), "s"
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=randint(0, 100))
-    clf = LinearSVC(C=0.1,class_weight='balanced')
-    t0 = time()
-    clf.fit(X_train, y_train)
-    print "training time:", round(time() - t0, 3), "s"
-    # predict
-    t0 = time()
-    pred = clf.predict(X_test)
-    print "predicting time:", round(time() - t0, 3), "s"  # accuracy
-    print accuracy_score(y_test, pred)
+    for i in xrange(5):
+        print 'run ', i + 1
+        Params = {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000]}
+        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=randint(0, 100))
+        clf = grid_search.GridSearchCV(LinearSVC(class_weight='balanced'), Params, cv=3)
+        t0 = time()
+        clf.fit(X_train, y_train)
+        print "training time:", round(time() - t0, 3), "s"
+        print 'best estimator after 5 fold CV: ', clf.best_estimator_
+        # predict
+        t0 = time()
+        pred = clf.predict(X_test)
+        print "predicting time:", round(time() - t0, 3), "s"  # accuracy
+        print "accuracy: ",accuracy_score(y_test, pred)
 
 
 if __name__ == "__main__":
