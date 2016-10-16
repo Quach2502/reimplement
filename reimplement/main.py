4dd517a80186fd8d4f12dc87bf9263262b83c66f
DungQuach <quachdung2502@gmail.com>
2016-10-16 23:42:34 +0800

    fix

--- reimplement/main.py
+++ reimplement/main.py
@@ -1,13 +1,13 @@
 import re
-from nltk.corpus import sentiwordnet as swn
 import nltk
+import string
+import numpy as np
+import networkx as nx
 from nltk import ngrams
 from time import time
-from nltk.parse.stanford import StanfordParser
 from nltk.parse.stanford import StanfordDependencyParser
 from sklearn.svm import LinearSVC
-import string
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import Normalizer
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import accuracy_score
@@ -15,15 +15,18 @@ from nltk.corpus import stopwords
 from xml.etree import ElementTree as ET
 from scipy.sparse import csr_matrix
 from scipy.sparse import hstack
-import numpy as np
 from sklearn import grid_search
 from random import randint
 
-# path_to_jar = 'C:\Users\t_quacd\AppData\Local\stanford-parser-full-2015-12-09/stanford-parser.jar'
-# path_to_models_jar = 'C:\Users\t_quacd\AppData\Local\stanford-parser-full-2015-12-09/stanford-parser-3.6.0-models.jar'
-# parser = StanfordParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
-# dep_parser = StanfordDependencyParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
-#
+# Call Stanford Parser by nltk, set up is follow the link http://stackoverflow.com/questions/13883277/stanford-parser-and-nltk
+# Set up JAVAHOME to jre , CLASSPATH environment variable to run
+# Example in this machine:
+# CLASSPATH = D:\stanford-parser-full-2015-12-09\stanford-parser-full-2015-12-09\stanford-parser.jar;D:\stanford-parser-full-2015-12-09\stanford-parser-full-2015-12-09\stanford-parser-3.6.0-models.jar
+# JAVAHOME = C:\Program Files\Java\jre1.8.0_102\bin
+
+dep_parser = StanfordDependencyParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
+
+# We keep negation words in stop words
 stopwords = [var for var in stopwords.words('english') if var not in ['not', 'isn']]
 
 
@@ -32,6 +35,7 @@ def NRC_lexicon():
         sentences = f.readlines()
         dict = {}
         for sentence in sentences:
+            # Choose only the word with sentimental labeled as 'positive' or 'negative'
             if (('1' in sentence) & (('positive' in sentence) | ('negative' in sentence))):
                 sentence = sentence.translate(string.maketrans("\t", " "))
                 sentence = sentence.translate(string.maketrans("\n", " "))
@@ -49,7 +53,8 @@ def MPQA_lexicon():
                 sentence = sentence.translate(string.maketrans("=", " "))
                 sentence = sentence.translate(string.maketrans("\n", " "))
                 sentence = sentence.split(' ')
-                if ((sentence[-2] == ('positive')) | (sentence[-2] == 'negative')):  # ignore neutral word for now
+                # Ignore neutral word for now
+                if ((sentence[-2] == ('positive')) | (sentence[-2] == 'negative')):
                     dict[sentence[5]] = sentence[-2]
     return dict
 
@@ -69,15 +74,13 @@ def BL_lexicon():
     return dict
 
 
-# def sentimental_lexicon_id(): #in domain lexicon
-
-
-def surface_context(sentence, aspect_term, from_char, to_char,
-                    window_size):  # from_index is the begin of the aspect term in list sentence.strip()
+def position_aspectTerm(sentence, aspect_term, from_char,
+                        to_char):  # Return the position of aspect term in a given sentence with given from/to positions of character
     list_sentence = sentence.split()
     from_index = 0
     to_index = 0
     position = 0
+    # from_index and to_index is the position of the aspect term in the list
     for each in list_sentence:
         if (position + len(each) + 1) <= int(from_char):
             position += len(each) + 1
@@ -85,6 +88,7 @@ def surface_context(sentence, aspect_term, from_char, to_char,
             from_index = list_sentence.index(each)
             break
     position = 0
+    # if the aspect term is only 1 word so from_index = to_index
     if len(aspect_term.split()) == 1:
         to_index = from_index
     else:
@@ -94,6 +98,13 @@ def surface_context(sentence, aspect_term, from_char, to_char,
             else:
                 to_index = list_sentence.index(each)
                 break
+    return from_index, to_index
+
+
+def surface_context(sentence, aspect_term, from_char, to_char,
+                    window_size):  # from_char, to_char is the begin/ end of the aspect term in list sentence.split() extracted from data
+    list_sentence = sentence.split()
+    from_index, to_index = position_aspectTerm(sentence, aspect_term, from_char, to_char)
     if (from_index - window_size > 0):
         window_begin = from_index - window_size
     else:
@@ -104,11 +115,43 @@ def surface_context(sentence, aspect_term, from_char, to_char,
         window_end = len(list_sentence)
     # print window_begin,window_end
     window = list_sentence[window_begin:window_end]
-    return window
+    return window  # Return a list of n words surrounding the aspect term, n is window_size
 
 
-# def parse_context(sentence, from_index, to_index):
-#     return result
+def parse_context(sentence, aspect_term, from_char, to_char):
+    from_index, to_index = position_aspectTerm(sentence, aspect_term, from_char, to_char)
+    source_fromIndex = str(from_index + 1)
+    source_toIndex = str(to_index + 1)
+    parse_sent = dep_parser.raw_parse(sentence)
+    dep = parse_sent.next()
+    G = nx.Graph()
+    context = []
+    for each in dep.to_dot().split("\n")[4:-1]:  # Use index [4:-1] to get the text only
+        # Convert the input from unicode to text
+        each = str(each)
+        # Remove all the punctuations except ">","=" to identify the relationship in the graph
+        each = each.translate(string.maketrans("", ""), string.punctuation.replace(">", "").replace("=", ""))
+        # If there is ">" the text is about the relationship( edge)
+        if ">" in each:
+            relationship = each.split("=")[-1]
+            each = each.split()
+            G.add_edge(each[0], each[2], label=relationship)
+        else:
+            each = each.split()
+            G.add_node(each[0], name=each[-1])
+    name_attribute = nx.get_node_attributes(G, 'name')
+    parse_context_fromIndex = nx.single_source_shortest_path_length(G, source_fromIndex, cutoff=3)
+    for each in parse_context_fromIndex.keys():
+        condition = int(each)
+        if (condition > 0 and (condition <= (from_index+1) or condition >= (to_index+1))):
+            context.append(name_attribute[each])
+    if (to_index != from_index):
+        parse_context_toIndex = nx.single_source_shortest_path_length(G, source_toIndex, cutoff=3)
+        for each in parse_context_toIndex.keys():
+            condition = int(each)
+            if (condition > 0 and (condition < (from_index + 1) or condition > (to_index + 1))):
+                context.append(name_attribute[each])
+    return list(set(context))  # Return the nodes (words) in the parse tree that are connected to the aspect term by at most three edges
 
 
 def lexicon_feature(sentence):
@@ -118,13 +161,10 @@ def lexicon_feature(sentence):
     tokens = nltk.word_tokenize(sentence)
     number_pos_MPQA = 0
     number_neg_MPQA = 0
-    sum_sentimental_MPQA = 0
     number_pos_BL = 0
     number_neg_BL = 0
-    sum_sentimental_BL = 0
     number_pos_NRC = 0
     number_neg_NRC = 0
-    sum_sentimental_NRC = 0
 
     for token in tokens:
         if token in MPQA_dict.keys():
@@ -150,7 +190,7 @@ def lexicon_feature(sentence):
     return result
 
 
-def sentence_transform(sentence, aspect_term, from_char, to_char, window_size):
+def surface_feature(sentence, aspect_term, from_char, to_char, window_size):
     window = surface_context(sentence, aspect_term, from_char, to_char, window_size)
     sentence_window = ' '.join(window)
     slice = sentence_window.partition(aspect_term)
@@ -158,6 +198,7 @@ def sentence_transform(sentence, aspect_term, from_char, to_char, window_size):
     for i in range(0, len(window)):
         result.append(window[i])
     for i in range(0, len(window)):
+        # Determine whether the word is before or after the aspect term to form the correct bigrams context target
         if window[i] in slice[0]:
             result.append(window[i] + "_" + aspect_term + "_ct")
         elif window[i] in slice[2]:
@@ -166,10 +207,36 @@ def sentence_transform(sentence, aspect_term, from_char, to_char, window_size):
     for bigram in bigrams:
         result.append(bigram[0] + "_" + bigram[1])
     result.append(aspect_term + "_at")
-    return ' '.join(result)
+    return result  # Return a list of surface features
 
 
-# def parse_feature(sentence):
+def parse_feature(sentence,aspect_term,from_char,to_char):
+    context = parse_context(sentence,aspect_term,from_char,to_char)
+    # dict_postag = {'word1':'POSTAG1','word2':'POSTAG2'}
+    result = []
+    dict_postag = {}
+    postag = nltk.pos_tag(nltk.word_tokenize(sentence))
+    # Create a dictionary with words as keys and POS_TAG as values
+    for each in postag:
+        dict_postag[each[0]] = each[1]
+    for each in context:
+        result.append(each + '_'+ dict_postag[each]) # Word_POSTAG in the parse context
+    slice = sentence.partition(aspect_term)
+    for word in context:
+        # Determine whether the word is before or after the aspect term to form the correct bigrams context target
+        if word in slice[0]:
+            result.append(word + "_" + aspect_term + "_ct")
+        elif word in slice[2]:
+            result.append(aspect_term + "_" + word + "_ct")
+
+
+
+def sentence_transform(sentence, aspect_term, from_char, to_char, window_size):
+    surfaceFeats = surface_feature(sentence, aspect_term, from_char, to_char, window_size)
+    parseFeats = parse_feature(sentence, aspect_term, from_char, to_char)
+    return ' '.join(surfaceFeats + parseFeats)
+
+
 def GetYFromStringLabels(Labels):
     Y = []
     for L in Labels:
@@ -201,22 +268,15 @@ def preprocessData():
                                                                                          string.punctuation),
                                                aspectTerm.get("from"), aspectTerm.get("to"), 10))
                 polarity.append(aspectTerm.get("polarity"))
-    LexFeats = [lexicon_feature(Sent) for Sent in text]
+    LexFeats = [lexicon_feature(sent) for sent in text]
     LexFeats = np.array(LexFeats)
     LexFeats = csr_matrix(LexFeats)
-    CountVecter = CountVectorizer(dtype=np.float64, binary=False, max_df=0.95, stop_words=stopwords)
+    cv = CountVectorizer(dtype=np.float64, binary=False, max_df=0.95, stop_words=stopwords)
     Y = GetYFromStringLabels(polarity)
-    X = CountVecter.fit_transform(text)
+    X = cv.fit_transform(text)
     X = Normalizer().fit_transform(X)
     X = hstack([X, LexFeats])
     return X, Y
-    # vectorizer
-    # vectorizer = TfidfVectorizer(tokenizer=tokenize, sublinear_tf=True, ngram_range=(1, 1),
-    #                              stop_words=stopwords,
-    #                              max_df=0.8)
-    # features_train_transformed = vectorizer.fit_transform(features_train).toarray()
-    # features_test_transformed = vectorizer.transform(features_test).toarray()
-    # return features_train_transformed, features_test_transformed, labels_train, labels_test
 
 
 def main():
@@ -236,7 +296,7 @@ def main():
         t0 = time()
         pred = clf.predict(X_test)
         print "predicting time:", round(time() - t0, 3), "s"  # accuracy
-        print "accuracy: ",accuracy_score(y_test, pred)
+        print "accuracy: ", accuracy_score(y_test, pred)
 
 
 if __name__ == "__main__":
